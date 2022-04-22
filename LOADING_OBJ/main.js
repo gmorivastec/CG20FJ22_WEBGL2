53ead83d568e2c3da6fcf504272b6c65e3b080b2
Guillermo Rivas <grivas@pop-os.localdomain>
2022-04-22 18:16:37 -0500

    april 22 class

--- LOADING_OBJ/main.js
+++ LOADING_OBJ/main.js
@@ -9,49 +9,78 @@ loader.load(
     'pyramid.obj', 
     function(data){
 
+        // I need a place to store my vertices 
+        // we are going to fill this up with the 'v's
+        var verticesSource = [];
+
+        // We are going to need the actual vertices to be processed
+        // We are going to fill this up with the 'f's
+        var processedVertices = [];
+
         var lines = data.split('\n');
+
+        // looping through the lines 
         for(let i = 0; i < lines.length; i++){
-            console.log(lines[i]);
+            
+            var currentLine = lines[i].split(' ');
+            
+            if(currentLine[0] == "v"){
+                
+                verticesSource.push(new Array(currentLine[2], currentLine[4], currentLine[6]));
+            }
+
+            if(currentLine[0] == "f"){
+                console.log(currentLine);
+
+                for(let j = 2; j <= 6; j += 2){
+
+                    // retrieve the current vertex in the current face
+                    var currentVertex = verticesSource[currentLine[j] - 1];
+                    
+                    currentVertex.forEach(element => {
+                        processedVertices.push(element);
+                    });
+                }
+            }
         }
-        console.log(data);
+
+        console.log(verticesSource);
+        console.log(processedVertices);
+
+        // we must create a scene in three.js in order to display something
+        const scene = new THREE.Scene();
+
+        // the camera
+        // perspective - 
+        // render volume - whatever falls inside will be shown
+        // frustum
+        const camera = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.1, 1000);
+        camera.position.set(10, -10, 10);
+        camera.lookAt(0, 0, 0);
+
+        const renderer = new THREE.WebGLRenderer();
+
+        // setup renderer
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        document.body.appendChild(renderer.domElement);
+
+        // we are going to load a mesh from vertices
+        const vertices = new Float32Array(processedVertices); 
+
+        const geometry = new THREE.BufferGeometry();
+        geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
+        const material = new THREE.MeshBasicMaterial({ color: 0xFF00FF });
+        const mesh = new THREE.Mesh(geometry, material);
+
+        scene.add(mesh);
+        renderer.render(scene, camera);
     }, 
     function(progress){
         //
-        console.log(progress);
+        // console.log(progress);
     }, 
     function(e){
         console.error(e);
     }
     );
 
-// we must create a scene in three.js in order to display something
-const scene = new THREE.Scene();
-
-// the camera
-// perspective - 
-// render volume - whatever falls inside will be shown
-// frustum
-const camera = new THREE.PerspectiveCamera(15, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.set(0, 0, 10);
-camera.lookAt(0, 0, 0);
-
-const renderer = new THREE.WebGLRenderer();
-
-// setup renderer
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-// we are going to load a mesh from vertices
-const vertices = new Float32Array([
-    -1.0, -1.0, 1.0,
-     1.0, -1.0, 1.0,
-     1.0,  1.0, 1.0
-]); 
-
-const geometry = new THREE.BufferGeometry();
-geometry.setAttribute('position', new THREE.BufferAttribute(vertices, 3));
-const material = new THREE.MeshBasicMaterial({ color: 0xFF00FF });
-const mesh = new THREE.Mesh(geometry, material);
-
-scene.add(mesh);
-renderer.render(scene, camera);
